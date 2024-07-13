ef5ebc2c95e0a5dbf650af0f7d7dc84c8e99d4b9
Hexerse <lucaschuat@hotmail.com>
2024-07-13 18:05:13 +0800

    fetched data

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-import data from './data.json' assert { type: 'json' };
+let jsonData;
 
-console.log(data);
 
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    jsonData = data;
+
+    processData(jsonData);
+  })
+  .catch(error => console.error('Error loading JSON:', error));
+
+
+function processData(data){
+  console.log(data)
+}
