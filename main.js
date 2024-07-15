ae8c7fcd27e4bc3dfddc0e3fc390c9aa6dcdc09e
Hexerse <lucaschuat@hotmail.com>
2024-07-15 15:23:24 +0800

    Before toying with DOM manipulation

--- main.js
+++ main.js
@@ -1,32 +1,44 @@
 let jsonData;
+const containerHTML = document.body
+const div = document.createElement("div")
+containerHTML.append(div)
+containerHTML
+.innerHTML = ` 
+<div class="container">
+  <div class="userContainer">
+    <div class="userReport">
+      <img src="./images/image-jeremy.png" alt="">
+        <p class="reportfor"> <span class="smallreportfor">Report for</span> <br> <span class="namePlate">Jeremy Robson</span></p>
+    </div>
+    <div class="options">
+      <button class="daily">Daily</button>
+      <button class="weekly">Weekly</button>
+      <button class="monthly">Monthly</button>
+    </div>
+  </div>
+` 
 
 
 fetch('./data.json')
+  
   .then(response => response.json())
   .then(data => {
     console.log(data);
     jsonData = data;
 
-    processData(jsonData);
+    loophtml(jsonData);
   })
   .catch(error => console.error('Error loading JSON:', error));
 
 
-let item;
+  function loophtml (jsonData){
+    jsonData.forEach(element => {
+      console.log(element);
+      console.log (containerHTML)
+      
 
-function processData(data){
-  console.log(data);
-  //Filters work
 
-  const dataWork = data.filter((data) => {
-    return data.title === 'Work'
-  })
-  console.log (dataWork);
+    });
+  }
 
-  const dataPlay = data.filter((data) => {
-    return data.title === 'Play'
-  })
-  console.log (dataPlay);
-
-}
 
